e2f27102e93aeb7f957f12debad45744248f8d36
Viwu <sinconsistencia@gmail.com>
2025-12-06 10:20:46 -0600

    logging

--- source/mpac/backend/Logging.hx
+++ source/mpac/backend/Logging.hx
@@ -2,13 +2,56 @@ package mpac.backend;
 
 import flixel.FlxG;
 
+typedef PrintDataWithType =
+{
+	> PrintData,
+	type:String,
+}
+
+typedef PrintData =
+{
+	?class_id:String
+}
+
 class Logging
 {
-	public static function info(data:Dynamic)
+	public static function log(data:Dynamic, ?print_data:PrintDataWithType)
 	{
-		#if FEATURE_TRACES
-		trace(data);
+		var log:String = '';
+
+		if (print_data != null)
+		{
+			log += '\t[\t' + print_data.type.toUpperCase();
+
+			if (print_data.class_id != null)
+				log += ' // ' + print_data.class_id;
+
+			log += '\t]\t:\t';
+		}
+
+		log += data;
+
+		#if CONSOLE_LOGS
+		Sys.println(log);
 		#end
-		FlxG.log.add(data);
+
+		FlxG.log.add(log);
 	}
+
+	public static function logTypeBase(type:String, ?data:Dynamic, ?print_data:PrintData)
+	{
+		log(data, {
+			type: type,
+			class_id: print_data.class_id
+		});
+	}
+
+	public static function info(data:Dynamic, ?print_data:PrintData)
+		logTypeBase("info", data, print_data);
+
+	public static function error(data:Dynamic, ?print_data:PrintData)
+		logTypeBase("error", data, print_data);
+
+	public static function warning(data:Dynamic, ?print_data:PrintData)
+		logTypeBase("warning", data, print_data);
 }
