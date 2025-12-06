fc36c859d25fc0ea39057b521e0f105b89408da6
Viwu <sinconsistencia@gmail.com>
2025-12-06 11:00:27 -0600

    itll look nicer

--- source/mpac/backend/Logging.hx
+++ source/mpac/backend/Logging.hx
@@ -21,12 +21,12 @@ class Logging
 
 		if (print_data != null)
 		{
-			log += '[' + print_data.type.toUpperCase();
+			log += '[ ' + print_data.type.toUpperCase();
 
 			if (print_data.class_id != null)
 				log += ' | ' + print_data.class_id;
 
-			log += '] : ';
+			log += ' ] : ';
 		}
 
 		log += data;
