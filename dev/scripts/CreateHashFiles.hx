0e2b9a982fbf11f7d6ae518804c433454a491e20
Viwu <sinconsistencia@gmail.com>
2025-12-06 12:13:37 -0600

    thats alot o traces

--- dev/scripts/CreateHashFiles.hx
+++ dev/scripts/CreateHashFiles.hx
@@ -18,16 +18,19 @@ class CreateHashFiles
 		FileSystem.createDirectory(path);
 		var count:Int = Std.int(Math.random() * 500);
         var total_count = count;
+		
+		trace('Creating ' + total_count + ' hash files');
 
 		while (count > 0)
 		{
 			var hash:String = Sha256.encode('h: ' + Math.random() * 463);
 			sys.io.File.saveContent(path + hash + '.hashfile', hash);
-			trace(path + hash + '.hashfile' + ' (' + ((total_count - count) + 1) + '/' + (total_count) + ')');
 
 			count--;
 		}
-		var alphabet = ' abcdefghijklmnopqrstuvwxyz';
-		sys.io.File.saveContent(path + alphabet.charAt(Std.int(Math.random() * alphabet.length)) + '.hashfile', 'Anomaly is lose. Find and capture it.');
+		var symbols = ' abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()`~-=_+[]\\{}|;\':",./<>?';
+		sys.io.File.saveContent(path + symbols.charAt(Std.int(Math.random() * symbols.length)) + '.hashfile', 'Anomaly is lose. Find and capture it.');
+		
+		trace('Finished creating hash files');
 	}
 }
